bd1b0388e1a5ba684910e1d43c3bdfb0d72fa2e1
chicofullstack <chico_fullstack_2023@outlook.com>
2024-06-29 19:27:59 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        padding: 0 10px;
+        background-color: #AAAAFF;
+        font-size: 36px;
+        font-family: 'Roboto';
+        text-align: right;
+        color: white;
+        border: 0;
+    }
+`
